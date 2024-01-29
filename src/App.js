d9640ed7b06805ab0ed494494607f6e9febf54cf
SagarTeli1999 <wreckerteli1999@gmail.com>
2024-01-29 19:53:34 +0530

    first command

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Digital from  "./Digital";
+import Demo from  "./Demo";
+import "./index.css";
+const App =() =>{
+return <Digital/>
+return<Demo/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+};
 
 export default App;
---
+++ src/Demo.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./index.css";
+
+const Demo = () => {
+  return (
+    <div classname="parentclass">
+      <img src="C:/Users/Sagar/Downloads/digitalflake.jpeg"/>
+      <h1>Welcome to DigitalflakeAdmin</h1>
+    </div>
+  );
+};
+
+export default Demo;
